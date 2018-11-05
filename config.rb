310d800e9b602b3fd6efbe8e41a3410e979dddd4
Samuel Beca <samuel.beca@gmail.com>
2018-11-05 16:42:19 +0100

    started on flat dynamic view

--- config.rb
+++ config.rb
@@ -20,3 +20,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+["steveo", "lindsey", "pia"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
